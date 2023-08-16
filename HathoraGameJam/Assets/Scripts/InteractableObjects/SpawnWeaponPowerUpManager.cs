a7c61a6f42d04677ba8284dbeb07db9fb73fcb09
VKChiuselli <vittorio.chiuselli@gmail.com>
2023-08-17 00:41:51 +0200

    Create spawn object manager

--- HathoraGameJam/Assets/Scripts/InteractableObjects/SpawnWeaponPowerUpManager.cs
+++ HathoraGameJam/Assets/Scripts/InteractableObjects/SpawnWeaponPowerUpManager.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class SpawnWeaponPowerUpManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<Transform> spawnPoints = new List<Transform>();
+    public List<GameObject> listOfObjectToSpawn;
     void Start()
     {
-        
+        spawnPoints = new List<Transform>();
+
+        foreach (Transform spawnPoint in this.transform)
+        {
+            spawnPoints.Add(spawnPoint);
+        }
+        lastSpawnTime = Time.time;
+
+        SpawnObject();
+        SpawnObject();
+        SpawnObject();
+        SpawnObject();
+        SpawnObject();
+        SpawnObject();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public float spawnDelay = 15f;
+
+    private float lastSpawnTime;
+
+
+
+
+    private void Update()
     {
-        
+        if (Time.time - lastSpawnTime >= spawnDelay)
+        {
+            lastSpawnTime = Time.time;
+            SpawnObject();
+        }
     }
+
+    private void SpawnObject()
+    {
+        if (spawnPoints.Count == 0)
+        {
+            return;
+        }
+        int randomIndex = Random.Range(0, spawnPoints.Count);
+        int randomItemIndex = Random.Range(0, listOfObjectToSpawn.Count);
+        Transform spawnPoint = spawnPoints[randomIndex];
+
+        Instantiate(listOfObjectToSpawn[randomItemIndex], spawnPoint.position, spawnPoint.rotation);
+        spawnPoints.Remove(spawnPoint);
+
+    }
+
 }
