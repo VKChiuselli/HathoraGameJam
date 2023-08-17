058f19a2dcf2fe9a5d89bea94c2033418eae5c3a
mrphilipjoel <39815836+mrphilipjoel@users.noreply.github.com>
2023-08-17 12:16:54 -0400

    Update PatrolPoints.cs

--- HathoraGameJam/Assets/Scripts/PatrolPoints.cs
+++ HathoraGameJam/Assets/Scripts/PatrolPoints.cs
@@ -8,12 +8,27 @@ public class PatrolPoints : MonoBehaviour
     [SerializeField]    private int currentPoint = -1;
     [SerializeField] List<Transform> pointsWhereBossGo; //make this enumerator
     [SerializeField] public Transform deskPoint; //make this enumerator
+    [SerializeField] Transform bossWaypointsParent;
 
 
     private void Awake()
     {
-    
+        if (pointsWhereBossGo.Count < 1)
+        {
+            GetBossWaypoints();
+        }
+    }
 
+    private void GetBossWaypoints()
+    {
+        pointsWhereBossGo.Clear(); // Clear the list before populating
+
+        // Loop through all children of bossWaypointsParent and add them to pointsWhereBossGo list
+        for (int i = 0; i < bossWaypointsParent.childCount; i++)
+        {
+            Transform child = bossWaypointsParent.GetChild(i);
+            pointsWhereBossGo.Add(child);
+        }
     }
 
     public Transform GetNext()
